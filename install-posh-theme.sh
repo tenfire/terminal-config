e90d6b956ee7400cb0bb8176b4054555000b6435
Erik Nilsson <nil_erik@hotmail.com>
2023-11-25 18:42:42 +0100

    Fix remove if the files does not exist

--- install-posh-theme.sh
+++ install-posh-theme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm ~/poshtheme.omp.json
+rm -f ~/poshtheme.omp.json
 ln -s $(pwd)/poshtheme.omp.json ~/poshtheme.omp.json
 echo 'test -f ~/poshtheme.omp.json && eval "$(oh-my-posh --init --shell bash --config ~/poshtheme.omp.json)"' >> ~/.bashrc
