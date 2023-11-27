6d34fa6a1d7db8b2d67ebf1a91229039ef43a135
Erik Nilsson <nil_erik@hotmail.com>
2023-11-27 11:44:20 +0100

    Install posh theme for zsh

--- install-posh-theme.sh
+++ install-posh-theme.sh
@@ -3,3 +3,4 @@
 rm -f ~/poshtheme.omp.json
 ln -s $(pwd)/poshtheme.omp.json ~/poshtheme.omp.json
 echo 'test -f ~/poshtheme.omp.json && eval "$(oh-my-posh --init --shell bash --config ~/poshtheme.omp.json)"' >> ~/.bashrc
+echo 'test -f ~/poshtheme.omp.json && eval "$(oh-my-posh --init --shell zsh --config ~/poshtheme.omp.json)"' >> ~/.zshrc
