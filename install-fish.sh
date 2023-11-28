c9d30ab58fbea1ef6400a38f98fe1569479ca43a
Erik Nilsson <nil_erik@hotmail.com>
2023-11-28 12:42:51 +0100

    install software-properties-common

--- install-fish.sh
+++ install-fish.sh
@@ -2,6 +2,7 @@ if ! command -v fish &> /dev/null
 then
   echo "fish not installed"
   echo "installing fish"
+  apt install software-properties-common -y
   apt-add-repository ppa:fish-shell/release-3 -y
   apt update
   apt install fish -y
