0c99b2ed022ba9fd6108b1cb1ae80986ede144a0
Erik Nilsson <nil_erik@hotmail.com>
2023-11-29 20:12:26 +0100

    Add instructions for installing fish on turenas scale

--- install-fish.sh
+++ install-fish.sh
@@ -12,3 +12,18 @@ fi
 # echo "deb http://deb.debian.org/debian bullseye-backports main contrib non-free" | tee -a /etc/apt/sources.list.d/backports.list
 # apt update
 # apt install fish/bullseye-backports
+
+# For truenas-scale
+# make dpkg executable
+#   chmod +x /usr/bin/dpkg
+# download .deb file from https://fishshell.com/
+# try to install fish:
+#   install deb -i <fish-deb-file>
+# if it failed, libpcre2 is probably missing:
+#   download deb file for libpcre2:
+#     install deb -i <libpcre2-deb-file>
+#   try to install fish again:
+#     install deb -i <libpcre2-deb-file>
+# set default shell to fish from the user interface at Credentials -> Local Users
+# make dpkg not executable again
+#   chmod -x /usr/bin/dpkg
