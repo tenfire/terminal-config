84f051d975d18bec85a8686e57593d404049efa4
Erik Nilsson <nil_erik@hotmail.com>
2023-12-05 11:13:59 +0100

    Auto-load posh theme when starting fish

--- install-posh-theme.sh
+++ install-posh-theme.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-rm -f ~/poshtheme.omp.json
-ln -s $(pwd)/poshtheme.omp.json ~/poshtheme.omp.json
-echo 'test -f ~/poshtheme.omp.json && eval "$(oh-my-posh --init --shell bash --config ~/poshtheme.omp.json)"' >> ~/.bashrc
-echo 'test -f ~/poshtheme.omp.json && eval "$(oh-my-posh --init --shell zsh --config ~/poshtheme.omp.json)"' >> ~/.zshrc
+BASHRC_FILE=~/.bashrc
+ZSHRC_FILE=~/.zshrc
+FISH_CONFIG_FILE=~/.config/fish/interactive.fish
+THEME_SRC=$(pwd)/poshtheme.omp.json
+THEME_DEST=~/poshtheme.omp.json
 
-# For fish. Add this to ~/.config/fish/config.fish
-# oh-my-posh --init --shell fish --config ~/poshtheme.omp.json | source
+rm -f "$THEME_DEST"
+ln -s "$THEME_SRC" "$THEME_DEST"
+
+insert () {
+  if ! grep -qxF "$1" "$2"; then
+    echo "$1" >> "$2"
+    echo "Appended \"$1\" to $2"
+  fi
+}
+
+insert "test -f \"$THEME_DEST\" && eval \"\$(oh-my-posh --init --shell bash --config "$THEME_DEST")\"" "$BASHRC_FILE"
+
+insert "test -f \"$THEME_DEST\" && eval \"\$(oh-my-posh --init --shell zsh --config "$THEME_DEST")\"" "$ZSHRC_FILE"
+
+insert "test -f \"$THEME_DEST\" && oh-my-posh --init --shell fish --config \"$THEME_DEST\" | source" "$FISH_CONFIG_FILE"
